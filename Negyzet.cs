63fab73ffb77b39ed2243792887d83fb3174e922
SigMaZsola <catfishzsolti@gmail.com>
2026-01-08 13:37:53 +0100

    Első classok

---
+++ Negyzet.cs
@@ -0,0 +1,26 @@
+﻿public class Negyzet
+{
+    bool empty;
+    string color;
+    int indexX;
+    int indexY;
+    Babu babu;
+
+    public Negyzet(int x, int y, string color)
+    {
+        indexX = x;
+        indexY = y;
+        this.color = color;
+        empty = true;
+        babu = null;
+    }
+
+    public Negyzet(int x, int y, string color, Babu babu)
+    {
+        indexX = x;
+        indexY = y;
+        this.color = color;
+        empty = false;
+        this.babu = babu;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace feladat
+{
+
+    public class Program
+    {   
+        Negyzet[,] tabla = new Negyzet[8, 8];
+        public static void Main(string[] args)
+        {
+            megtoltTabla();
+        }
+
+        public static void megtoltTabla() {
+            for (int) { 
+            }
+        }
+    }
+}
